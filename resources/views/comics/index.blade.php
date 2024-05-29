3eab14e1dec89e378c67ffac6bee93e7f8df058b
ChriCore99 <christian.corelli.io@gmail.com>
2024-05-29 16:31:08 +0200

    ADD: prima parte del crud

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,3 @@
+<h1>
+    vista index di DC COMICS
+</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComicsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+// Route::get('/',['App\Http\Controllers\ComicsController', 'index']);
+
+Route::get('/',[ComicsController::class, 'index']);
